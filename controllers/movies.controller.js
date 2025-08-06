14c2df51238dfb6db839fbbf0a62a2632fb630bd
karvendhanst <imkarvendhan@gmail.com>
2025-08-06 22:54:01 +0300

    final commit

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,9 +1,22 @@
+import Movie from "../models/movie.model.js";
+
 export const movieIndex = (req, res)=>{
     res.send("Get Movies List")
 } 
 
-export const movieCreate = (req, res)=>{
-    res.send("Create Movie")
+export const movieCreate = async(req, res)=>{
+    const newMovie = new Movie({
+        title: req.body.title,
+        desc: req.body.desc
+    })
+
+    try{
+        const movie = await newMovie.save()
+        return res.status(201).json(movie)
+    }
+    catch(error){
+        return res.status(400).json({ message: error.message})
+    }
 } 
 
 export const movieUpdate = (req, res)=>{
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const movieRoutes = require("./routes/movies.route")
+const db = require("./lib/db")
 
 const PORT = 3000;
+db()
 
 app.use(express.json())
+
 
 app.get("/", (req, res)=>{
     res.json({msg : "From Express"})
